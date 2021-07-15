4ac3d8374c561e551a453bdc70a8154366defe66
Maithil Wade <wmaithil@gmail.com>
2021-07-15 23:43:00 +0530

    first-commit

---
+++ src/provider.js
@@ -0,0 +1,32 @@
+
+import React, {useState} from 'react';
+
+import PackageContext from './context';
+
+const Provider = props => {
+    const [mission , setMission] = useState({
+        mname : "Go To abc",
+        agent : "007",
+        accept:"Not accepted"
+    });
+
+    return(
+
+        <PackageContext.Provider
+        value = {{
+                    data: mission,
+                    isMissionAccepted: ()=>
+                    {
+                        setMission({...mission, accept: "ACCEPTED" });
+                    }
+                }}
+        >
+            
+        { props.children }
+
+        </PackageContext.Provider>
+    );
+
+};  
+
+export default Provider;
